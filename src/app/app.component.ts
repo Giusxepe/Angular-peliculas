f9fa57dee242b00c160e78fe850d61b59f088f81
Alberto Favela <betosft_12@hotmail.com>
2021-11-14 15:01:32 -0600

    version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+// OnInit permite ejecutar una funcinalidad antes de que se cargue el componente
+
+export class AppComponent implements OnInit {
+  // setTimeout tiene la funcionalidad de agregar un contador de tiempo 
+  //antes de que se cargue el contenido de ngOnInit()
+  ngOnInit(): void {
+    setTimeout(() => { 
+      this.peliculasEnCines= [{
+        titulo: 'spiderman',
+        fechadelanzamiento: new Date(),
+        precio: 1400.99,
+        poster: 'https://www.imdb.com/title/tt0145487/mediaviewer/rm3632146944/?ref_=tt_ov_i'
+      },
+      {
+        titulo: 'Moana',
+        fechadelanzamiento: new Date('2016-11-14'),
+        precio: 300.33,
+        poster:'https://www.imdb.com/title/tt3521164/mediaviewer/rm618728448/?ref_=tt_ov_i'
+      }]
+    }, 3000);
+  }
   title = 'front-end';
+
+  duplicarNumero(valor: number): number{
+    return valor * 2
+  }
+
+  ocultar = false;
+
+  peliculasEnCines;
+  peliculasProximosEstrenos = [];
+
+  manejarRated(voto: number): void{
+    alert(voto);
+  }
 }
---
+++ src/app/utilidades/rating/rating.component.ts
@@ -0,0 +1,57 @@
+import { Component, Input, EventEmitter, OnInit, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-rating',
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.css']
+})
+export class RatingComponent implements OnInit {
+
+  @Input()
+  maximoRating=5;
+  @Input()
+  ratingSeleccionado=0;
+  @Output()
+  rated: EventEmitter<number> =  new EventEmitter<number>();
+  
+  maximoRatingArr=[];
+  votado = false;
+  ratingAnterior;
+
+  
+  constructor() { }
+
+  ngOnInit(): void {
+
+   // Instancia un Array con numero de elementos igual a la variable maximoRating, rellenandolo de 0s 
+    this.maximoRatingArr = Array(this.maximoRating).fill(0);
+
+  }
+
+  manejarMouseEnter(index: number): void{
+
+    this.ratingSeleccionado = index + 1;
+  }
+
+  manejarMouseLeave(index: number): void{
+      
+    if(this.ratingAnterior !== 0){
+
+        this.ratingSeleccionado = this.ratingAnterior;
+    }
+    else{
+      this.ratingSeleccionado = 0;
+    }
+    
+  }
+
+  rate(index:number):void{
+    this.ratingSeleccionado = index + 1;
+    this.votado=true;
+    this.ratingAnterior=this.ratingSeleccionado;
+
+    this.rated.emit(this.ratingSeleccionado);
+  }
+
+}
